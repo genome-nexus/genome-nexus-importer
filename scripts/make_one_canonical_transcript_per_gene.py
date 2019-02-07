ce6edea075730e6f50276f519fe5149c72578e5c
Sander Tan <sandertan@thehyve.nl>
2019-02-07 13:18:06 +0100

    Add GRCh38 data with correct HGNC columns

--- scripts/make_one_canonical_transcript_per_gene.py
+++ scripts/make_one_canonical_transcript_per_gene.py
@@ -28,6 +28,7 @@ def pick_canonical_longest_transcript_from_ensembl_table(ensembl_rows, field):
     no such thing, pick biggest gene id"""
     return ensembl_rows.sort_values('is_canonical protein_length gene_stable_id'.split(), ascending=False)[field].values[0]
 
+
 def get_ensembl_canonical(ensembl_rows, field):
     if (ensembl_rows.ndim == 1 and len(ensembl_rows) == 0) or (ensembl_rows.ndim == 2 and len(ensembl_rows) == 0):
         return np.nan
@@ -62,11 +63,14 @@ def get_ensembl_canonical_transcript_id_from_hgnc_then_ensembl(ensembl_table, hg
     else:
         raise(Exception("One hugo symbol expected in hgnc_canonical_genes"))
 
+
 def lowercase_set(x):
     return set({i.lower() for i in x})
 
+
 def ignore_rna_gene(x):
     return set({i for i in x if not i.startswith('rn') and not i.startswith('mir') and not i.startswith('linc')})
+
 
 def ignore_certain_genes(x):
     ignore_genes = {'fam25hp', 'rmrpp1', 'hla-as1', 'pramef16', 'arid4b-it1',
@@ -84,8 +88,8 @@ def main(ensembl_biomart_geneids_transcript_info,
          ensembl_biomart_canonical_transcripts_per_hgnc):
 
     # input files
-    tsv = pd.read_csv(ensembl_biomart_geneids_transcript_info, sep='\t', dtype={'is_canonical':bool})
-    tsv = tsv.drop_duplicates()
+    transcript_info_df = pd.read_csv(ensembl_biomart_geneids_transcript_info, sep='\t', dtype={'is_canonical':bool})
+    transcript_info_df = transcript_info_df.drop_duplicates()
     uniprot = pd.read_csv(isoform_overrides_uniprot, sep='\t')\
         .rename(columns={'enst_id':'isoform_override'})\
         .set_index('gene_name'.split())
@@ -95,41 +99,55 @@ def main(ensembl_biomart_geneids_transcript_info,
     custom = pd.read_csv(isoform_overrides_genome_nexus, sep='\t')\
         .rename(columns={'enst_id':'isoform_override'})\
         .set_index('gene_name'.split())
-    hgnc = pd.read_csv(hgnc_complete_set, sep='\t', dtype=object)
-    hgnc = hgnc[hgnc['name'] != 'entry withdrawn'].copy()
-    hugos = hgnc['symbol'].unique()
-    hgnc = hgnc.set_index('symbol')
+    hgnc_df = pd.read_csv(hgnc_complete_set, sep='\t', dtype=object)
+
+    # Convert new column names to old stable column names. If this is not done properly, Genome Nexus and any other
+    # downstream applications break
+    column_name_mapping = {'name': 'approved_name',
+                           'symbol': 'approved_symbol',
+                           'prev_symbol': 'previous_symbols',
+                           'alias_symbol': 'synonyms',
+                           'location': 'chromosome',
+                           'entrez_gene_id': 'entrez_id',
+                           'ena': 'accession_numbers',
+                           'refseq_accession': 'refseq_ids',
+                           'uniprot_ids': 'uniprot_id'}
+    hgnc_df.rename(columns=column_name_mapping, inplace=True)
+
+    hgnc_df = hgnc_df[hgnc_df['approved_name'] != 'entry withdrawn'].copy()
+    hugos = hgnc_df['approved_symbol'].unique()
+    hgnc_df = hgnc_df.set_index('approved_symbol')
     # assume each row has approved symbol
-    assert(len(hugos) == len(hgnc))
+    assert(len(hugos) == len(hgnc_df))
 
     # only test the cancer genes for oddities (these are very important)
     cgs = set(pd.read_csv('common_input/oncokb_cancer_genes_list_20170926.txt',sep='\t')['Hugo Symbol'])
     # each cancer gene stable id should have only one associated cancer gene symbol
-    assert(tsv[tsv.hgnc_symbol.isin(cgs)].groupby('gene_stable_id').hgnc_symbol.nunique().sort_values().nunique() == 1)
+    assert(transcript_info_df[transcript_info_df.hgnc_symbol.isin(cgs)].groupby('gene_stable_id').hgnc_symbol.nunique().sort_values().nunique() == 1)
     # each transcript stable id always belongs to only one gene stable id
-    assert(tsv.groupby('transcript_stable_id').gene_stable_id.nunique().sort_values().nunique() == 1)
+    assert(transcript_info_df.groupby('transcript_stable_id').gene_stable_id.nunique().sort_values().nunique() == 1)
 
     # create hgnc_symbol to gene id mapping
-    # ignore hugo symbols from ensembl data dump (includes prev symbols and alias_symbol)
-    syns = hgnc.alias_symbol.str.strip('"').str.split("|").dropna()
+    # ignore hugo symbols from ensembl data dump (includes prev symbols and synonyms)
+    syns = hgnc_df.synonyms.str.strip('"').str.split("|").dropna()
     syns = set(itertools.chain.from_iterable(syns))
-    prev_symbol = hgnc.prev_symbol.str.strip('"').str.split("|").dropna()
-    prev_symbol = set(itertools.chain.from_iterable(prev_symbol))
+    previous_symbols = hgnc_df.previous_symbols.str.strip('"').str.split("|").dropna()
+    previous_symbols = set(itertools.chain.from_iterable(previous_symbols))
 
-    # there is overlap between symbols, alias_symbol and previous symbols
+    # there is overlap between symbols, synonyms and previous symbols
     # therefore use logic in above order when querying
-    # assert(len(syns.intersection(prev_symbol)) == 0) #329
+    # assert(len(syns.intersection(previous_symbols)) == 0) #329
     # assert(len(set(hugos).intersection(syns)) == 0) # 495
-    # assert(len(set(hugos).intersection(prev_symbol)) == 0) #227
+    # assert(len(set(hugos).intersection(previous_symbols)) == 0) #227
 
     # all cancer genes and hugo symbols in ensembl data dump should be
-    # contained in hgnc approved symbols and alias_symbol
+    # contained in hgnc approved symbols and synonyms
     # c12orf9 is only in sanger's cancer gene census and has been withdrawn
-    assert(len(lowercase_set(set(cgs)) - set(['c12orf9']) - lowercase_set(set(hugos).union(syns).union(prev_symbol))) == 0)
-    no_symbols_in_hgnc = lowercase_set(tsv.hgnc_symbol.dropna().unique()) - lowercase_set(set(hugos).union(syns).union(prev_symbol))
+    assert(len(lowercase_set(set(cgs)) - set(['c12orf9']) - lowercase_set(set(hugos).union(syns).union(previous_symbols))) == 0)
+    no_symbols_in_hgnc = lowercase_set(transcript_info_df.hgnc_symbol.dropna().unique()) - lowercase_set(set(hugos).union(syns).union(previous_symbols))
     assert(len(ignore_certain_genes(ignore_rna_gene(no_symbols_in_hgnc))) == 0)
 
-    tsv = tsv.set_index('hgnc_symbol')
+    transcript_info_df = transcript_info_df.set_index('hgnc_symbol')
     # for testing use
     # NSD3 replaces WHSC1L1
     # AATF has uniprot canonical transcript, not hgnc ensembl gene id, but
@@ -138,11 +156,11 @@ def main(ensembl_biomart_geneids_transcript_info,
     one_transcript_per_hugo_symbol = pd.Series(hugos).apply(lambda x:
         pd.Series(
             [
-                get_ensembl_canonical_transcript_id_from_hgnc_then_ensembl(tsv, x, hgnc, 'gene_stable_id'),
-                get_ensembl_canonical_transcript_id_from_hgnc_then_ensembl(tsv, x, hgnc, 'transcript_stable_id'),
-                get_overrides_transcript([custom], tsv, x, hgnc),
-                get_overrides_transcript([uniprot, custom], tsv, x, hgnc),
-                get_overrides_transcript([mskcc, uniprot, custom], tsv, x, hgnc),
+                get_ensembl_canonical_transcript_id_from_hgnc_then_ensembl(transcript_info_df, x, hgnc_df, 'gene_stable_id'),
+                get_ensembl_canonical_transcript_id_from_hgnc_then_ensembl(transcript_info_df, x, hgnc_df, 'transcript_stable_id'),
+                get_overrides_transcript([custom], transcript_info_df, x, hgnc_df),
+                get_overrides_transcript([uniprot, custom], transcript_info_df, x, hgnc_df),
+                get_overrides_transcript([mskcc, uniprot, custom], transcript_info_df, x, hgnc_df),
             ],
             index="""
             ensembl_canonical_gene
@@ -157,8 +175,8 @@ def main(ensembl_biomart_geneids_transcript_info,
     one_transcript_per_hugo_symbol.index.name = 'hgnc_symbol'
 
     # merge in other hgnc fields
-    merged = pd.merge(one_transcript_per_hugo_symbol.reset_index(), hgnc.reset_index(), left_on='hgnc_symbol', right_on='symbol')
-    del merged['symbol']
+    merged = pd.merge(one_transcript_per_hugo_symbol.reset_index(), hgnc_df.reset_index(), left_on='hgnc_symbol', right_on='approved_symbol')
+    del merged['approved_symbol']
     del merged['ensembl_gene_id']
     merged.to_csv(ensembl_biomart_canonical_transcripts_per_hgnc, sep='\t', index=False)
 
