c05146ff831461d541262188b8f6d8547bdebbaf
Pieter Lukasse <pieterlukasse@gmail.com>
2022-08-25 15:12:08 +0200

    updated generate_updated_grch38_hotspots_info and tests

--- scripts/hotspots/update_hotspots_to_grch38.py
+++ scripts/hotspots/update_hotspots_to_grch38.py
@@ -30,20 +30,31 @@ import argparse
 from xmlrpc.client import boolean
 import requests, sys
 import pandas as pd
- 
+import logging
+import math
+
 ENSEMBL_GRCH38_SERVER = "https://rest.ensembl.org"
 ENSEMBL_GRCH37_SERVER = "https://grch37.rest.ensembl.org"
 CBIOPORTAL_SERVER = "https://www.cbioportal.org"
 GENOME_NEXUS_GRCH38_SERVER = "https://grch38.genomenexus.org"
 
+protein_sequence_cache = {}
 
 def get_translated_protein_sequence(ensembl_server: str, transcript_id: str) -> str:
-    """ Returns the translated protein sequence for the given transcript id
+    """ Returns the translated protein sequence for the given transcript id.
+        Returns None if the webservice failed with a 400 type code.
     """
+    cache_key = "{0}_{1}".format(ensembl_server, transcript_id)
+    if cache_key in protein_sequence_cache:
+        return protein_sequence_cache[cache_key]
     api_url = "{0}/sequence/id/{1}?type=protein".format(ensembl_server, transcript_id) 
     response = requests.get(api_url, headers={ "Content-Type" : "text/plain"})
     if not response.ok:
-       response.raise_for_status()
+        if response.status_code >= 400 and response.status_code < 500:
+            return None
+        else:
+            response.raise_for_status()
+    protein_sequence_cache[cache_key] = response.text
     return response.text
 
 
@@ -51,52 +62,92 @@ def get_gene_and_transcript_map(hugo_and_transcript_id_file_name: str) -> dict[s
     """ Returns a map of hugo symbol X canonical transcript id.
     """
     # parse file w/ pandas and build the result map:
-    hugo_and_transcript_df = pd.read_csv(hugo_and_transcript_id_file_name, sep='\t')
+    hugo_and_transcript_df = pd.read_csv(hugo_and_transcript_id_file_name, sep='\t', dtype=str)
     hugo_and_transcript_df.set_index("hgnc_symbol", inplace=True)
     hugo_and_transcript_map = hugo_and_transcript_df.to_dict(orient="index")
     return hugo_and_transcript_map
 
 
-def validate_new_transcript_id(grch37_transcript_id: str,
+def new_transcript_id_is_valid(grch37_transcript_id: str,
                                proposed_grch38_transcript_id: str) -> boolean:
     """Compares the translated sequence of the grch37 transcript vs the grch38 one
-       and returns True if the sequences match
+       and returns True if the sequences match.
+       Returns False if the sequences do not match or if the comparison was not
+       possible (e.g. because the transcript id one either side is invalid)
     """
     grch37_sequence = get_translated_protein_sequence(ENSEMBL_GRCH37_SERVER, grch37_transcript_id)
     grch38_sequence = get_translated_protein_sequence(ENSEMBL_GRCH38_SERVER, proposed_grch38_transcript_id)
     return grch37_sequence == grch38_sequence
 
-def generate_updated_grch38_hotspots_file(hugo_and_transcript_id_file_name: str,
-                                          hotspots_2d_3d_grch37_file_name: str,
-                                          isoform_override_source: str) -> None:
-    hugo_and_transcript_map = get_gene_and_transcript_map(hugo_and_transcript_id_file_name)
-    hotspots_grch37_df = pd.read_csv(hotspots_2d_3d_grch37_file_name, sep='\t')
+
+def get_new_hugo_symbol(old_hugo_symbol: str, hugo_and_transcript_map: str) -> str:
+    """Looks for the give old_hugo_symbol in  "previous_symbols" or "synonyms" columns of the map values
+       and when found, returns the corresponding key (the new hugo_symbol). If not found, returns None.
+    """
+    for new_hugo_symbol, other_columns in hugo_and_transcript_map.items():
+        if type(other_columns['previous_symbols']) == str:
+            previous_symbols = [x.strip() for x in other_columns['previous_symbols'].split(',')]
+            if old_hugo_symbol in previous_symbols:
+                logging.info("found {0} in 'previous_symbols", old_hugo_symbol)
+                return new_hugo_symbol
+        if type(other_columns['synonyms']) == str:
+            synonyms = [x.strip() for x in other_columns['synonyms'].split(',')]
+            if old_hugo_symbol in synonyms:
+                logging.info("found {0} in 'synonyms", old_hugo_symbol)
+                return new_hugo_symbol
+    return None
+
+
+def find_grch38_transcript_id(hugo_symbol: str, hugo_and_transcript_map: dict, grch38_isoform_override_source: str) -> str:
+    """Tries to find the hugo symbol in the map's index or in the alias column. If found,
+       returns the respective transcript id according to grch38_isoform_override_source.
+       If not found, returns None.
+    """
+    if hugo_symbol not in hugo_and_transcript_map:
+        # then try to find the row that has this symbol as one of the values in "previous_symbols" or "synonyms"
+        hugo_symbol = get_new_hugo_symbol(hugo_symbol, hugo_and_transcript_map)
+        if hugo_symbol is None:
+            return None
+          
+    return hugo_and_transcript_map[hugo_symbol][grch38_isoform_override_source + "_canonical_transcript"]
+
+
+def generate_updated_grch38_hotspots_info(grch38_hugo_and_transcript_id_file_name: str,
+                                          grch38_isoform_override_source: str,
+                                          grch37_hotspots_2d_3d_file_name: str) -> None:
+    hugo_and_transcript_map = get_gene_and_transcript_map(grch38_hugo_and_transcript_id_file_name)
+    grch37_hotspots_df = pd.read_csv(grch37_hotspots_2d_3d_file_name, sep='\t', dtype=str)
     
     # iterate over the rows and replace with new transcript_id if valid:
-    for index, row in hotspots_grch37_df.iterrows():
+    for index, row in grch37_hotspots_df.iterrows():
         hugo_symbol = row['hugo_symbol']
+        logging.info("Processing {0} ...", hugo_symbol)
         grch37_transcript_id = row['transcript_id']
-        proposed_grch38_transcript_id = hugo_and_transcript_map[hugo_symbol][isoform_override_source + "_canonical_transcript"]
-        is_valid = validate_new_transcript_id(grch37_transcript_id,
+        proposed_grch38_transcript_id = find_grch38_transcript_id(hugo_symbol, hugo_and_transcript_map, grch38_isoform_override_source)
+        is_valid = proposed_grch38_transcript_id is not None and \
+                   new_transcript_id_is_valid(grch37_transcript_id,
                                               proposed_grch38_transcript_id)
         # if valid, replace transcript_id:
         if is_valid:
-            row['transcript_id'] = proposed_grch38_transcript_id
+            if row['transcript_id'] != proposed_grch38_transcript_id:
+                logging.info('Updating transcript of {0} from {1} to {2}', hugo_symbol, row['transcript_id'], proposed_grch38_transcript_id)
+                row['transcript_id'] = proposed_grch38_transcript_id
         else:
             # drop any row where new transcript_id is not valid:
-            hotspots_grch37_df.drop(index, inplace=True)
+            logging.info('Dropping entry for {0}', hugo_symbol)
+            grch37_hotspots_df.drop(index, inplace=True)
     
-    # write the dataframe out as a new grch38 file:
-    # TODO
+    # return the updated dataframe:
+    return grch37_hotspots_df
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("--hugo_and_transcript_id_file_name", default="../../data/grch38_ensembl95/export/ensembl_biomart_canonical_transcripts_per_hgnc.txt",
+    parser.add_argument("--grch38_hugo_and_transcript_id_file_name", default="../../data/grch38_ensembl95/export/ensembl_biomart_canonical_transcripts_per_hgnc.txt",
                         type=str, help="hugo symbols with respective transcript ids mapping file")
-    parser.add_argument("--isoform_override_source", default="mskcc",
+    parser.add_argument("--grch38_isoform_override_source", default="mskcc",
                         type=str, help="which transcript override source is preferred. Possible values: [mskcc, uniprot, genome_nexus, ensembl]")
-    parser.add_argument("--hotspots_2d_3d_grch37_file_name", default="../../data/grch37_ensembl92/export/hotspots_v2_and_3d.txt",
+    parser.add_argument("--grch37_hotspots_2d_3d_file_name", default="../../data/grch37_ensembl92/export/hotspots_v2_and_3d.txt",
                         type=str, help="combined grch37 2D and 3D cancerhotspots data file")
     args = parser.parse_args()
 
--- scripts/unit_test_transformations.py
+++ scripts/unit_test_transformations.py
@@ -12,7 +12,7 @@ import os
 import transform_gff_to_tsv_for_exon_info_from_ensembl
 import gzip
 import hotspots.update_hotspots_to_grch38
-
+import requests
 
 class TransformTestCase(unittest.TestCase):
     """Superclass for testcases that test the transformation steps.
@@ -55,9 +55,50 @@ class TransformTestCase(unittest.TestCase):
         self.assertEqual('ENST00000646891', hugo_and_transcript_map['BRAF']['mskcc_canonical_transcript'])
         self.assertEqual('ENST00000288602', hugo_and_transcript_map['BRAF']['uniprot_canonical_transcript'])
 
-    def test_validate_new_transcript_id(self):
-        result = hotspots.update_hotspots_to_grch38.validate_new_transcript_id('ENST00000288602', 'ENST00000646891')
+    def test_new_transcript_id_is_valid(self):
+        result = hotspots.update_hotspots_to_grch38.new_transcript_id_is_valid('ENST00000288602', 'ENST00000646891')
         self.assertTrue(result)
 
-        result = hotspots.update_hotspots_to_grch38.validate_new_transcript_id('ENST00000275493', 'ENST00000646891')
+        result = hotspots.update_hotspots_to_grch38.new_transcript_id_is_valid('ENST00000275493', 'ENST00000646891')
         self.assertFalse(result)
+
+    def test_get_translated_protein_sequence(self):
+        result = hotspots.update_hotspots_to_grch38.get_translated_protein_sequence(hotspots.update_hotspots_to_grch38.ENSEMBL_GRCH37_SERVER,
+                                                                                    'ENST00000288602')
+        self.assertIsNotNone(result)
+        result = hotspots.update_hotspots_to_grch38.get_translated_protein_sequence(hotspots.update_hotspots_to_grch38.ENSEMBL_GRCH38_SERVER,
+                                                                                    'ENST_invalid')
+        self.assertIsNone(result)
+        
+        # expect exception:
+        with self.assertRaises(requests.exceptions.ConnectionError):
+            hotspots.update_hotspots_to_grch38.get_translated_protein_sequence(hotspots.update_hotspots_to_grch38.ENSEMBL_GRCH38_SERVER+'err',
+                                                                                    '')
+
+
+    def test_generate_updated_grch38_hotspots_info(self):
+        result_df = hotspots.update_hotspots_to_grch38.generate_updated_grch38_hotspots_info('data/grch38_ensembl95/export/ensembl_biomart_canonical_transcripts_per_hgnc.txt',
+                                                                                             'mskcc',
+                                                                                             'scripts/test_files/grch37_ensembl92/export/small_hotspots_v2_and_3d.txt')
+                                                                                             #'data/grch37_ensembl92/export/hotspots_v2_and_3d.txt')
+        # iterate over all rows and check if each expected change occured, e.g. for BRAF:
+        # TODO
+
+        # Does NOT work, because the gene occurs in multiple lines:
+        result_df.set_index("hugo_symbol", inplace=True)
+        hugo_based_map = result_df.to_dict(orient="index")
+        self.assertEqual('ENST00000646891', hugo_based_map['BRAF']['transcript_id'])
+
+
+    def test_find_grch38_transcript_id(self):
+        hugo_and_transcript_map = hotspots.update_hotspots_to_grch38.get_gene_and_transcript_map('data/grch38_ensembl95/export/ensembl_biomart_canonical_transcripts_per_hgnc.txt')
+        # or test wit a mock:
+        #  hugo_and_transcript_map = {
+        #     'BRAF': {'previous_symbols': 'BLA,ETC', 'synonyms': None},
+        #     'NEW_H3F3A': {'previous_symbols': 'BLA, H3F3A', 'synonyms': None, 'mskcc_canonical_transcript': 'ENST00000366815'},
+        # }
+        transcript_id = hotspots.update_hotspots_to_grch38.find_grch38_transcript_id('H3F3A', hugo_and_transcript_map, 'mskcc')
+        self.assertEqual('ENST00000366815', transcript_id)
+
+        transcript_id = hotspots.update_hotspots_to_grch38.find_grch38_transcript_id('invalidhugo', hugo_and_transcript_map, 'mskcc')
+        self.assertIsNone(transcript_id)
