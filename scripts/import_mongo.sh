188da7bf8497fc8f1333b07ae6cacd2b35c3225a
Xiang Li <xiangli5699@gmail.com>
2024-08-21 19:33:15 -0400

    Change unxz to xz command

--- scripts/import_mongo.sh
+++ scripts/import_mongo.sh
@@ -59,7 +59,7 @@ curl https://genome-nexus-static-data.s3.amazonaws.com/mutationassessor4_for_gen
 echo "Download completed."
 
 echo "Extracting Mutation assessor data"
-unxz -k ${DIR}/../data/common_input/mutationassessor4_for_genome_nexus.tsv.xz
+xz -dk ${DIR}/../data/common_input/mutationassessor4_for_genome_nexus.tsv.xz
 
 echo "Transforming Mutation assessor data"
 sed -i '' 's/uniprotId\tSV\thgvspShort\tF_score\tF_impact\tMSA\tMAV/uniprotId\tsv\thgvspShort\tf_score\tf_impact\tmsa\tmav/' ${DIR}/../data/common_input/mutationassessor4_for_genome_nexus.tsv
